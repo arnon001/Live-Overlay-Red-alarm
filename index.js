896612dd7ab488655cb13813c6b1e098bd14c49c
Arnon Hacohen <arnonhacohen123@gmail.com>
2024-11-10 19:35:54 +0200

    Reverted since kore.co.il API didn't worked

--- index.js
+++ index.js
@@ -1,26 +1,39 @@
 const express = require('express');
+const pikudHaoref = require('pikud-haoref-api');
 const app = express();
 const PORT = process.env.PORT || 1234;
-const axios = require('axios');
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-const interval = 500; // Polling every 0.5 seconds
+const interval = 750; // Polling every 0.5 seconds
 let currentAlert = null;
+const pollForAlerts = () => {
+  const options = {
+    // proxy: 'http://user:pass@hostname:port/' (if needed outside Israel)
+  };
 
+  pikudHaoref.getActiveAlert((err, alert) => {
+    setTimeout(pollForAlerts, interval);
 
-var poll = function () {
-  axios.get('https://kore.co.il/redAlert.json')
-  .then(response => {
-    if(currentAlert != response.data) {
-      console.log(response.data);
+    if (err) {
+      console.error('Error retrieving active alert:', err);
+      return;
     }
-    currentAlert = response.data;
 
-    setTimeout(poll, interval);
-  })
-}
+    // Only update if there is a new alert
+    if (alert && JSON.stringify(alert) !== JSON.stringify(currentAlert)) {
+      console.log('New alert:', alert);
+    }
+
+    currentAlert = alert;// || null; // Set to null if no active alert
+  }, options);
+};
+
+// Start polling for alerts
+pollForAlerts();
+
+
 
 app.get('/', (req, res) => {
   res.render('index', { alert: currentAlert });
